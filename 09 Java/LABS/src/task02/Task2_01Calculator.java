75e239675cda8386f8dfdd478c1edab966d533a4
Andrei Leontev <leonand1985@gmail.com>
2022-10-17 21:29:36 +0300

    2.1

---
+++ 09 Java/LABS/src/task02/Task2_01Calculator.java
@@ -0,0 +1,21 @@
+package task02;
+
+public class Task2_01Calculator {
+    String name;
+    void calculate(double a, double b) { }
+    void calculate(long a, long b) { }
+    void calculate(int a, int b) { }
+
+     public int amount (int a, int b){
+         return a + b;
+     }
+    public int division (int a, int b){
+        return a / b;
+    }
+    public int multiplication (int a, int b){
+        return a * b;
+    }
+    public int subtraction (int a, int b){
+        return a - b;
+    }
+}
