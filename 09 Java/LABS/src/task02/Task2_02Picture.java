9351f7393ef338063c06d2c8900545e4d4f5008b
Andrei Leontev <leonand1985@gmail.com>
2022-10-19 19:19:14 +0300

    added 2.2

--- 09 Java/LABS/src/task02/Task2_02Picture.java
+++ 09 Java/LABS/src/task02/Task2_02Picture.java
@@ -5,7 +5,7 @@ public class Task2_02Picture {
     String artistName;
     int price;
 
-    //для известных картин
+    //конструктор для известных картин
     public Task2_02Picture(String title, String artistName, int price) {
         this.title = title;
         this.artistName = artistName;
@@ -18,7 +18,9 @@ public class Task2_02Picture {
 
     public static void main(String[] args) {
 
-        Task2_02Picture barsik = new Task2_02Picture("Black Square", "Malevich", 5000000);
-        Task2_02Picture streetCat = new Task2_02Picture();
+        Task2_02Picture blsq = new Task2_02Picture("Black Square", "Malevich", 5000000);
+        Task2_02Picture unknow = new Task2_02Picture();
+        System.out.println(blsq.title);
+        System.out.println(unknow.title);
     }
 }
