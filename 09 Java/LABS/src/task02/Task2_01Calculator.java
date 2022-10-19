71b3c185e60a19cfb818e5992dc42fb91c008c9e
Andrei Leontev <leonand1985@gmail.com>
2022-10-19 21:36:38 +0300

    changed 2.1

--- 09 Java/LABS/src/task02/Task2_01Calculator.java
+++ 09 Java/LABS/src/task02/Task2_01Calculator.java
@@ -1,24 +1,45 @@
 package task02;
 
 public class Task2_01Calculator {
-    String name;
-    void calculate(double a, double b) { }
-    void calculate(long a, long b) { }
-    void calculate(int a, int b) { }
-
-     public int amount (int a, int b){
-         return a + b;
-     }
-    public int division (int a, int b){
-
-        return a / b;
+    // сложение
+    public int amount(int a, int b) {
+        return a + b;
     }
-    public int multiplication (int a, int b){
+    public double amount(double a, double b) {
+        return a + b;
+    }public long amount(long a, long b) {
+        return a + b;
+    }
 
+    // деление
+    public double division(int a, int b) {
+        return (double) a / b;
+    }
+    public double division(double a, double b) {
+        return a / b;
+    }public double division(long a, long b) {
+        return (double) a / b;
+    }
+
+    // умножение
+    public int multiplication(int a, int b) {
         return a * b;
     }
-    public int subtraction (int a, int b){
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+    public long multiplication(long a, long b) {
+        return a * b;
+    }
 
+    // вычитание
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+    public long subtraction(long a, long b) {
         return a - b;
     }
 }
